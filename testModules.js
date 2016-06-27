88cbfaa7090ebc763d801a4c933a2e82499c33fe
bfarmilo <bfarmilo@gmail.com>
2016-06-27 16:29:41 -0400

    break out DB and html parser into modules, test node file

--- testModules.js
+++ testModules.js
@@ -1,66 +1,11 @@
-const electron = require('electron');
-// Module to control application life.
-const {app} = electron;
-// Module to create native browser window.
-const {BrowserWindow} = electron;
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win;
-
-function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({width: 1400, height: 800});
-
-  // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/index.html`);
-
-  // Open the DevTools.
-  //win.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-// npm modules needed
 var events = require('events');
 var fs = require('fs');
 
-var query = require('./js/DBconnect');
+var query = require('./js/app_DBconnect');
 var htmlparse = require ('./js/app_htmlparse');
 
 var eventEmitter = new events.EventEmitter();
 
-// once the server is hit -- before we hit the database, let's make the full Dropbox pathname
 function RunNewQuery(queryString, values) {
   // call the query module with querystring and then html parse the results
   console.log("querying DB ...");
@@ -69,22 +14,18 @@ function RunNewQuery(queryString, values) {
       console.log("Error with query ", err3);
       return;
     }
-    console.log("good query - results: %s", queryResults);
-    // call the htmlparse module with the Dropbox Path and callback writeFile
+    console.log("good query - "+queryResults.length+" results returned");
     console.log("parsing to html ...");
-    htmlparse(dropboxPath, queryResults, writeFile(err4, outputString));
+    htmlparse(queryResults, function(err4, outputString){
+      if (err4) {
+        console.log("Error Parsing query to HTML", err4);
+        return;
+      }
+      console.log("good html, writing file ...");
+      // htmlString contains the table-formatted query
+      fs.writeFile(outputFileName, outputString, FileWritten);
+    });
   });
-}
-
-
-function writeFile(err, htmlString) {
-  if (err) {
-    console.log("Error Parsing query to HTML", err);
-    return;
-  }
-  console.log("good html, writing file ...");
-  // htmlString contains the table-formatted query
-  fs.writeFile(outputFileName, htmlString, FileWritten);
 }
 
 
@@ -111,6 +52,7 @@ var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
 var queryJSON = {};
 var serve = serveStatic("./");
+var servePatents = serveStatic("./"); // just instantiate this for now, will replace with the dropbox path once we have it
 
 var thisresp = {}; // tracks the response object for the 'tableReady' checker.
 
@@ -128,7 +70,6 @@ var valString = [];
 
 // file lookups
 var dropboxPath = ""; // keeps the path to the local dropbox
-const relPath = "../../../" //gets us up to C:\ -- hack to make the patent links work
 const mainFileName = "index.html"; //the host file
 const outputFileName = "resultTable.html"; //the file we are writing to
 
@@ -146,8 +87,9 @@ function getDropBoxPath() {
       return;
     };
     // first, get the path to the local Dropbox folder and change the \ to /
-    dropboxPath = relPath + JSON.parse(pathdata).business.path.match(/:\\(.+)/)[1].replace(/\\/g, "/") + "/";
+    dropboxPath = "C\:/" + JSON.parse(pathdata).business.path.match(/:\\(.+)/)[1].replace(/\\/g, "/") + "/";
     console.log ("Good DropBox Path:", dropboxPath);
+    servePatents = serveStatic(dropboxPath); // serve anything with pdf in it from this directory instead
   });
 }
 
@@ -159,11 +101,13 @@ var server = http.createServer(function (request, response) {
   // the page requests 'tableReadycheck' after asking for a new query. Here we capture that
   if (request.url.indexOf("tableReadycheck") > -1 ) {
     thisresp = response;
+  } else if (request.url.indexOf("pdf") > -1) {
+    servePatents (request, response, done);
   } else {
     serve(request, response, done);
   };
 
-  // if the url is output.html then we need to parse the query parameters.
+  // if the url is mainFileName then we need to parse the query parameters.
 
   if (request.url.indexOf(mainFileName) > -1 ) {
 
@@ -174,6 +118,7 @@ var server = http.createServer(function (request, response) {
     if (propertyCheck.indexOf("meth") > -1) {
       // the parameter 'meth' is in the string.
       whereString = ""; //default - pull claim 1
+      valString=[]; //reset valString
 
       if (queryJSON.srch !=='' && queryJSON.srvl !== '') {
         // has a search and searchval we need to parse for the query
@@ -230,8 +175,8 @@ var server = http.createServer(function (request, response) {
         };
       }; // the parameters aren't blank
     }; // if 'meth' is in the query parameter
-    RunNewQuery(whereString, valString); //pass: whereString, valString, thisresp
-  }; //if the pathname is output.html
+    RunNewQuery(whereString, valString); // now update the results table
+  };
 }); // createserver
 
 // set up emitters, get Dropbox path, start server
