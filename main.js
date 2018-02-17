ea1c1e6c8b1ba23d3e9fd9b8d52e2218593697e2
Bill Farmilo <bill@farmilostrategy.com>
2018-02-16 23:17:51 -0500

    cleanup and migration to full preact back-end

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 const electron = require('electron');
-const { getDropBoxPath } = require('./js/getDropBoxPath');
-const dbquery = require('./js/app_DBconnect');
-// these are for old style queries
-const runNewQuery = require('./js/app_runNewQuery');
-const urlParse = require('./js/app_urlParse');
-const htmlparse = require('./js/app_htmlparsetempl');
-//
-const changeLog = require('./changeLog.json');
 const fse = require('fs-extra');
 const spawn = require('child_process').spawn;
+// local modules
+const { getDropBoxPath } = require('./js/getDropBoxPath');
+const { queryDatabase } = require('./js/app_DBconnect');
 const { connectDocker } = require('./js/connectDocker');
 const { getFullText } = require('./js/getFullText');
+const { parseQuery, parseOrder } = require('./js/app_sqlParse');
+// configuration
+const changeLog = require('./changeLog.json');
 const { patentDB } = require('./js/app_config.json');
-const { parseQuery } = require('./js/app_sqlParse');
 // developer
 const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 // other constants
@@ -21,12 +18,7 @@ let win;
 let markmanwin;
 let detailWindow = null;
 let connectParams;
-const savedSearch = {
-  where: '',
-  paramArray: [],
-}; // used for saved searches
 let dropboxPath = ''; // keeps the path to the local dropbox
-let queryType = '';
 let uriMode = process.env.USEDB === 'NextIdea'; // flag that indicates if claim HTML is uri-encoded or not
 
 connectDocker(patentDB.connection)
@@ -36,12 +28,22 @@ connectDocker(patentDB.connection)
   })
   .catch(err => console.error(err));
 
-function openPDF(fullPath) {
+// This method will be called when Electron has finished
+
+/** openPDF will try to open a pdf file using the shell
+ * 
+ * @param {String} fullPath
+ * @returns {void}
+ */
+const openPDF = (fullPath) => {
   console.log(`trying shell: ${dropboxPath}${fullPath}`);
   shell.openItem(dropboxPath + fullPath);
 }
 
-function createWindow() {
+/** createWindow launches the main window
+ *  @returns {void}
+ */
+const createWindow = () => {
   // get the file paths
   getDropBoxPath((err, dropbox) => {
     if (err) {
@@ -75,13 +77,9 @@ function createWindow() {
     if (markmanwin) markmanwin.close();
     win = null;
   });
-
-
 }
-
-// This method will be called when Electron has finished
+// Electron listeners
 // initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -98,22 +96,32 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+
+
 //Listener for launching patent details
 ipcMain.on('view_patentdetail', (event, patentNumber) => {
 
+  /** private function that sends new data to the patent detail window
+   * @param {Array<records>} newData - the JSON formatted data from the DB
+   * @returns {void}
+   */
   const updateRenderWindow = (newData) => {
     console.log('sending state for patent', newData.PMCRef);
     detailWindow.webContents.send('state', newData);
     detailWindow.show();
   }
 
-  const sendUpdate = () => {
+  /** private function that queries the DB then hits the USPTO if empty
+   * @returns {void}
+   */
+  const getPatentHtml = () => {
     console.log('got call for patent detail view with patent number', patentNumber);
-    dbquery(connectParams, 'p_PATENT', `WHERE PatentNumber=@0 FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER`, [patentNumber], (err, data) => {
+    queryDatabase(connectParams, 'p_PATENT', `WHERE PatentNumber=@0`, [patentNumber], ' FOR JSON AUTO, WITHOUT_ARRAY_WRAPPER', (err, data) => {
       if (err) {
         console.error(err)
       } else {
-        // TODO: if the query returns PatentHtml, serve it up,
+        // if the query returns PatentHtml, serve it up,
         // otherwise, go fetch it, serve it up, and meanwhile update the DB with it !
         // data returns the row in multiple columns of an array (if large)
         const state = JSON.parse(data.reduce((accum, item) => accum.concat(item), ''));
@@ -122,7 +130,8 @@ ipcMain.on('view_patentdetail', (event, patentNumber) => {
           getFullText(patentNumber)
             .then(PatentHtml => {
               state.PatentHtml = JSON.stringify(PatentHtml);
-              dbquery(connectParams, 'u_FULLTEXT', '', [state.PatentHtml, state.PatentID], (err2, status) => {
+              // now store the new data in the DB for future reference
+              queryDatabase(connectParams, 'u_FULLTEXT', '', [state.PatentHtml, state.PatentID], '', (err2, status) => {
                 if (err2) {
                   console.error(err2);
                 } else {
@@ -152,14 +161,21 @@ ipcMain.on('view_patentdetail', (event, patentNumber) => {
       detailWindow = null;
     });
     detailWindow.on('ready-to-show', () => {
-      console.log('window ready, calling sendUpdate');
-      sendUpdate();
+      console.log('window ready, calling getPatentHtml');
+      getPatentHtml();
     })
   } else {
-    sendUpdate();
+    getPatentHtml();
   }
 })
 
+// listener to handle when a user clicks on a patent link
+ipcMain.on('open_patent', (opEvent, linkVal) => {
+  console.log(`received link click with path ${linkVal}`);
+  openPDF(linkVal);
+});
+
+// Listener for manual closing of the patent window via X button
 ipcMain.on('close_patent_window', event => {
   detailWindow.close();
 })
@@ -198,114 +214,65 @@ ipcMain.on('add_claimconstructions', () => {
   // confirm if update not applied
   // close window
 });
-// Listener for updating a potential application
-ipcMain.on('update_application', (uaEvent, claimID, oldValues, newValues) => {
-  // log the change to allow future roll-backs
-  changeLog.changes.push({ datetime: Date.now(), claimID, from: oldValues, to: newValues });
-  fse.writeJSON('./changeLog.JSON', changeLog, 'utf8', (err) => {
-    if (err) { console.error(err) } else {
-      console.log(`changeLog updated: ${JSON.stringify(changeLog.changes[changeLog.changes.length - 1])}`);
-      // now do the insert query
-      dbquery(connectParams, 'u_UPDATE', claimID, newValues.split(), (err4, result) => {
-        if (err4) {
-          console.error(dialog.showErrorBox('Query Error', `Error with update query ${err4}`));
-        } else {
-          console.log('Potential Application', result);
-          win.send('ready');
-        }
-      });
-    }
-  });
-});
 
-ipcMain.on('update_watch', (uaEvent, claimID, oldValues, newValues) => {
-  // log the change to allow future roll-backs
-  changeLog.changes.push({ datetime: Date.now(), claimID, from: oldValues, to: newValues, column: 'WatchItems' });
-  fse.writeJSON('./changeLog.JSON', changeLog, 'utf8', (err) => {
-    if (err) { console.error(err) } else {
-      console.log(`changeLog updated: ${JSON.stringify(changeLog.changes[changeLog.changes.length - 1])}`);
-      // now do the insert query
-      dbquery(connectParams, 'u_WATCH', claimID, newValues.split(), (err4, result) => {
-        if (err4) {
-          console.error(dialog.showErrorBox('Query Error', `Error with update query ${err4}`));
-        } else {
-          console.log('Watch Item updated', result);
-          win.send('ready');
-        }
-      });
-    }
-  });
-});
+// Listener for changing to the other Database
+ipcMain.on('change_db', event => {
+  const newDB = connectParams.options.database === 'PMCDB' ? 'NextIdea' : 'PMCDB';
+  console.log('changing database to', newDB);
+  connectParams.options.database = newDB;
+  uriMode = newDB === 'NextIdea';
+})
 
-// listener to handle when a user clicks on a patent link
-ipcMain.on('open_patent', (opEvent, linkVal) => {
-  console.log(`received link click with path ${linkVal}`);
-  openPDF(linkVal);
-});
+// Listener for a call to update PotentialApplication or WatchItems
+ipcMain.on('json_update', (event, oldItem, newItem) => {
+  // items have structure patentNumber, index, claimID, field, value
+  changeLog.changes.push({ datetime: Date.now(), from: oldItem.value, to: newItem.value });
+  fse.writeJSON('./changeLog.json', changeLog, 'utf8')
+    .then(() => queryDatabase(connectParams, 'u_UPDATE', ` ${newItem.field}=@0 WHERE ClaimID=@1`, [newItem.value, parseInt(newItem.claimID, 10)], '', (err2, result) => {
+      if (err2) {
+        console.error(dialog.showErrorBox('Query Error', `Error with update query ${err2}`));
+      } else {
+        console.log('%s %s', newItem.field, result);
+      }
+    }))
+    .catch(err => console.error(err));
+})
 
+// Listener for a call to update the main window
 ipcMain.on('json_query', (event, query) => {
-  console.log('got query object %s', JSON.stringify(query, null, 1));
+  const orderBy = [
+    {field:"PatentNumber", direction:"ASC"}
+  ];
   //remove duplicates and create an array of ({field, value}) objects
   const fieldList = Object.keys(query).filter(item => query[item] !== '').map(item => ({ field: item, value: query[item] }));
+  console.log('reduced query to %s', JSON.stringify(fieldList));
   //make sure to URI-encode ClaimHtml if appropriate
   if (uriMode && fieldList.ClaimHtml) {
     fieldList.ClaimHtml = encodeURIComponent(fieldList.ClaimHtml).replace(/\'/g, '%27').replace('%', '[%]');
   }
   //in the case where a blank query is passed, default query is only show claim 1
   const parsedQuery = fieldList.length > 0 ? parseQuery(fieldList) : { where: 'claims.ClaimNumber LIKE @0', param: ['%'] };
-  dbquery(connectParams, 'p_SELECTJSON', `WHERE ${parsedQuery.where} FOR JSON AUTO`, parsedQuery.param, (err, result) => {
+  queryDatabase(connectParams, 'p_SELECTJSON', `WHERE ${parsedQuery.where}`, parsedQuery.param, `ORDER BY ${parseOrder(orderBy)} FOR JSON AUTO`, (err, result) => {
     if (err) {
       console.error(err);
     } else {
       // query comes down as an array of chunks. So need to join the chunks,
-      // Parse as JSON, then send to window
-      win.webContents.send('json_result', result.length > 0 ? JSON.parse(result.join('')) : '');
+      const queryResult = result.length > 0 ? JSON.parse(result.join('')) : '';
+      // for testing !
+      fse.writeJSON('./test/sampleResult.json', queryResult);
+      // Parse as JSON, but if uriMode decode the ClaimHtml first, then send to window
+      win.webContents.send('json_result', queryResult && uriMode ? queryResult.map(patent => ({
+        PMCRef: patent.PMCRef,
+        PatentNumber: patent.PatentNumber,
+        PatentPath: patent.PatentPath,
+        claims: patent.claims.map(claim => ({
+          ClaimID: claim.ClaimID,
+          ClaimNumber: claim.ClaimNumber,
+          PotentialApplication: claim.PotentialApplication,
+          WatchItems: claim.WatchItems,
+          ClaimHtml: decodeURIComponent(claim.ClaimHtml)
+        }))
+      })) : queryResult);
     }
   })
 });
-
-// listener to handle when a user launches a new query
-ipcMain.on('new_query', (opEvent, queryJSON) => {
-  // querystring comes back as {srch:'a%20OR%20b', srvl:} etc.
-  if (queryJSON.save !== true) {
-    // don't save the search -- clear the old values
-    savedSearch.where = '';
-    savedSearch.paramArray = [];
-  }
-  // in uriMode, need to convert % to [%] to parse SQL properly
-  // NOTE: Only do this for Uri encoded fields (namely, Claim full text only at this point)
-  if (uriMode && queryJSON.srch === 'Cl') queryJSON.srvl = encodeURIComponent(queryJSON.srvl).replace(/\'/g, '%27').replace('%', '[%]');
-  console.log(`new query received with parameters: ${JSON.stringify(queryJSON)}`);
-  if (queryJSON.srch.search('Te') !== -1 || queryJSON.srch.search('Co') !== -1 || queryJSON.srch.search('Nu') !== -1) {
-    queryType = 'm_MARKMANALL';
-  } else {
-    queryType = 'p_SELECT';
-  }
-  urlParse(queryJSON, savedSearch, (err5, whereClause, valueArray) => {
-    if (err5) {
-      console.error(dialog.showErrorBox('URL Parse Error', `Error parsing url parameters: ${queryJSON}\n ${err5}`));
-    } else {
-      console.log(whereClause, valueArray);
-      runNewQuery(connectParams, queryType, whereClause, valueArray, (err6, queryResults) => {
-        if (err6) {
-          console.error(dialog.showErrorBox('Query Error', `Error with query: ${whereClause} ${valueArray}\n ${err6}`));
-        } else {
-          console.log('parsing to html ...');
-          htmlparse(queryType, queryResults, uriMode, (err4, outputString) => {
-            if (err4) {
-              console.error(dialog.showErrorBox('HTML Parse Error', `Error Parsing query to HTML: ${err4}`));
-            } else {
-              console.log(`sending html-formatted table with ${queryResults.length} rows to renderer window`);
-              win.webContents.send('tableReady', outputString, queryResults.length); // now update the results table
-            }
-          });
-          if (queryJSON.save !== true) {
-            // only overwrite the savedSearch values if we haven't already saved some yet
-            savedSearch.where = whereClause; // save the last search for posterity
-            savedSearch.paramArray = valueArray; // save the last search for posterity
-          }
-        }
-      });
-    }
-  }); // urlParse
-}); // new_query handler
