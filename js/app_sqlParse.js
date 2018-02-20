d0154d55d084f8f5d04fb3948ff662ca51bd1d83
Bill Farmilo <bill@farmilostrategy.com>
2018-02-20 15:22:08 -0500

    improve documentation, get rid of old outdated code, added order by helper

--- js/app_sqlParse.js
+++ js/app_sqlParse.js
@@ -3,38 +3,13 @@
 const { urlParams, patentDB } = require('./app_config.json'); // stores the whereObj and regEx
 const matchExp = new RegExp(urlParams.reg, 'g');
 
-function sqlParse(searchField, saved, savedParamCount, searchString, callback) {
-  const parsedSearch = {
-    where: '',
-    param: [],
-  };
-  // if saved, we need to add one to all of the indeces
-  const delta = saved ? savedParamCount : 0;
-  try {
-    // if searchString is blank return all patents
-    // searchString is of the form word1 OR word2 AND word3
-    // could also be word1 OR NOT word2 AND NOT word3
-    // has at least one AND or OR
-    parsedSearch.param = searchString !== '' ? searchString.split(matchExp).map(values => `%${values}%`) : ['%'];
-    // now create a RegExp of the form /param1|param2|param3/g
-    // Note a search for A, N, D, O, R, T will mess up this regex. Hope they are at the start or end !
-    const newMatch = new RegExp(`^${parsedSearch.param.join('|').replace(/%/g, '')}$`, 'g');
-    console.log(newMatch);
-    // split the where into words like OR, AND, OR NOT, AND NOT
-    parsedSearch.where = searchString !== '' ? searchString.split(newMatch).map((values, index, array) => {
-      if (index === 0) return `${urlParams.whereObj[searchField]}${(values.search('NOT') !== -1) ? ' NOT' : ''} LIKE @${index + delta}`;
-      if (index === (array.length - 1)) return '';
-      return `${values.match(/ AND | OR /g)}${urlParams.whereObj[searchField]}${(values.search('NOT') !== -1) ? ' NOT' : ''} LIKE @${index + delta}`;
-    }).join('') : `${urlParams.whereObj[searchField]} LIKE @${delta}`;
-    return callback(null, parsedSearch.where, parsedSearch.param);
-  } catch (err) {
-    return callback(err);
-  }
-}
-
-
+/** expands a query item into a three element array
+ * @param {Object} query in the form of {field: value}
+ * @returns {Array<string, Array<string>, Array<string>>} first element is the field
+ * second element is an array of values, third element is an array of operators
+ */
 const expandValues = query => {
-  //convert a value into two arrays, one with parameters and one with operators
+
   //edge case - there are no & or | operators, so add an 'and' at the end
   if (!query.value.includes('&') && !query.value.includes('|')) return [query.field, [query.value], ['AND']];
   return [query.field].concat(query.value.replace(/&/g, '\\AND\\').replace(/\|/g, '\\OR\\').split('\\').reduce((parsed, item) => {
@@ -61,26 +36,23 @@ const flatten = list => list.reduce(
 */
 const parseQuery = query => {
 
-  /** expands a query item into a three element array
-   * @param {*} query in the form of {field: value}
-   * @returns {Array<string, Array<string>, Array<string>>} first element is the field
-   * second element is an array of values, third element is an array of operators
-   */
-
-  // the main function
+  //TODO: Handle NOT operator !
+  // create an expanded query, to deal with | and & in any query values
   const expandQuery = query.reduce((result, item) => {
     const expandedItem = expandValues(item);
     result.push(expandedItem);
     return result;
   }, []);
-
+  // now flatten it down and map it to a valid SQL where and parameter list
   return flatten(expandQuery.map(element => {
     // each element is [fieldname, [param list], [operator list]]
     // lookup the prefix and suffix associated with fieldName
     const parser = patentDB.fieldMap.filter(val => val.name === element[0])[0];
     // now parse the param list to create a where array and a param array
+    // for now, put in a placeholder ? to hold where the parameter index will go
+    // Make sure compound components are surrounded in brackets
     return element[1].map((item, index) => ({
-      where: `${parser.prefix}.${element[0]}${parser.suffix}@? ${element[2][index] || 'AND'}`,
+      where: `${index === 0 && element[1].length > 1 ? '(':''}${parser.prefix}.${element[0]}${parser.suffix}@? ${element[2][index] || ') AND'}`,
       param: `${parser.suffix === ' LIKE ' ? `%${item}%` : item}`,
     }));
   })).reduce((result, current, index) => {
@@ -101,17 +73,22 @@ const parseQuery = query => {
   }, {});
 }
 
+/** a simple function that creates a valid ORDER BY sql query
+ * @param {Array<{fieldname:string, direction:string}>}
+ * @returns {string} of the form 'table.Field ASC'
+ */
 const parseOrder = orderBy => {
-    // each element is [{fieldname, direction}]
-    // lookup the prefix and suffix associated with fieldName
-    return orderBy.map(element => {
-      const parser = element.field !== 'PatentNumber' ? patentDB.fieldMap.filter(val => val.name === element.field)[0] : {prefix:'patents'};
-      return `${parser.prefix}.${element.field} ${element.direction}`;
-    });
+  // each element is [{fieldname, direction}]
+  // lookup the prefix and suffix associated with fieldName
+  return orderBy.map(element => {
+    //hack, but with PatentNumber also add claim number
+    const parser = element.field !== 'PatentNumber' ? patentDB.fieldMap.filter(val => val.name === element.field)[0] : { prefix: 'patents' };
+    const direction = element.ascending ? 'ASC' : 'DESC';
+    return `${parser.prefix}.${element.field} ${direction}${element.field === 'PatentNumber' ? ', claims.ClaimNumber ASC' : ''}`;
+  });
 }
 
 module.exports = {
-  sqlParse,
   parseQuery,
   parseOrder
 };
