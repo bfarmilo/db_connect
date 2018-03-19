a6b67b5c095045b81cf882df50205160c267653c
Bill Farmilo <bill@farmilostrategy.com>
2018-03-19 08:01:42 -0400

    Research for retriving bibliographic data

--- test/7_getFullText.test.js
+++ test/7_getFullText.test.js
@@ -3,3 +3,39 @@ const { getFullText } = require('../js/getFullText');
 
 getFullText(8856375).then(result => console.log(result)).catch(err => console.error(err));
 
+// PTAB Pair Bulk Data
+/*
+PatentBulkData: [
+    applicationDataOrProsecutionHistoryDataOrPatentTermData: [
+        { ...
+        partyBag: [
+            {
+                applicantBagOrInventorBagOrOwnerBag: [
+                    ...
+                    { inventorOrDeceasedInventor: [
+                        {contactOrPublicationContact: [
+                            {
+                                name: {
+                                personNameOrOganizationNameOrEntityName: [
+                                    { personStructuredName: {
+                                        firstName,
+                                        middleName,
+                                        lastName
+                                    }}
+                                ]}
+                                cityName,
+                                geographicRegionName: ...
+                                countryCode
+                            }
+                        ],
+                        sequenceNumber
+                    }
+                    ]}
+                ]
+            }
+        ]}
+    ]
+]
+
+*/
+
