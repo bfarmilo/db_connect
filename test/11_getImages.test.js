1fb732067090200da64d7bbe8fc57a09b95a2a32
Bill Farmilo <bill@farmilostrategy.com>
2018-09-07 14:26:30 -0400

    moving on to testing imagemap feature

--- test/11_getImages.test.js
+++ test/11_getImages.test.js
@@ -12,7 +12,7 @@ let patentImages = new Map();
 // A simple test to verify a visible window is opened with a title
 chai.use(chaiAsPromised);
 
-describe('it can get a pdf image data from the USPTO', function () {
+describe('getAllImages', function () {
     /*     it('finds section 3', async () => {
             sectionTwoEnd = await findEndPage(docID, 3) - 1;
             console.log(sectionTwoEnd);
@@ -31,7 +31,7 @@ describe('it can get a pdf image data from the USPTO', function () {
             console.log(...testPages.keys());
             return expect(testPages.size).to.equal(6);
         }); */
-    it('can put it all together and do it automatically for a patent', async function () {
+    it('can get all images from the USPTO when given a valid patent number', async function () {
         const imageMap = new Map(await getAllImages(patentNo));
         expect(imageMap.size).to.equal(22);
         expect(imageMap.has(52)).to.be.true;
@@ -39,7 +39,7 @@ describe('it can get a pdf image data from the USPTO', function () {
         await fse.writeFile(`./test/page50`,imageMap.get(50).pageData);
         return console.log(imageMap.get(50));
     });
-    it('can put it all together and do it automatically for an application', async function () {
+    it('can get all images from the USPTO when given a valid application number', async function () {
         const imageMap = new Map(await getAllImages(applicationNo));
         expect(imageMap.size).to.equal(13);
         expect(imageMap.has(14)).to.be.true;
@@ -47,6 +47,11 @@ describe('it can get a pdf image data from the USPTO', function () {
         await fse.writeFile(`./test/page12`, imageMap.get(12).pageData);
         return console.log(imageMap.get(11));
     });
+    it('fails gracefully when a matching document is not found', async function () {
+        const imageMap = new Map(await getAllImages(10999999));
+        expect(imageMap.size).to.equal(0);
+        return console.log(imageMap)
+    })
 });
 
 describe('it can write image data to the DB', function () {
