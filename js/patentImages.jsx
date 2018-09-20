c87d06900e71319ca86e923c9c587dbdf3e23c44
Bill Farmilo <bill@farmilostrategy.com>
2018-09-20 12:49:46 -0400

    main patentImage component handles resizing of patent image Added next/prev buttons rewrite data handling to match query and tables Enabled rotation of images

--- js/patentImages.jsx
+++ js/patentImages.jsx
@@ -4,16 +4,20 @@ import { Icon } from './jsx/icons';
 import { PatentImage } from './jsx/PatentImageView';
 /** @jsx h */
 
+const CONTROL_HEIGHT = 25;
+
 class PatentImages extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            result: {
-                images: null
-            },
             patentImages: new Map(),
             currentImage: 0,
+            firstImage: 0,
+            prevEnabled: false,
+            lastImage: 0,
+            nextEnabled: true,
+            rotation: 0,
             windowSize: { width: 0, height: 0 }
         };
     }
@@ -25,29 +29,92 @@ class PatentImages extends Component {
     componentDidMount() {
         ipcRenderer.on('state', (event, data) => {
             console.log('received data', data);
-            // console.log('summaries', JSON.stringify(data.summaries));
-            const { images } = { ...data.images };
-            console.log(new Map(images), images[0][0]);
+            // Data format: ImageID, PatentID, PageNumber, ImageURL, PageData, Rotation
+            const { firstImage, lastImage } = data && data.reduce((extremes, current) => {
+                let { firstImage, lastImage } = extremes;
+                extremes.firstImage = current.PageNumber < firstImage ? current.PageNumber : firstImage;
+                extremes.lastImage = current.PageNumber > lastImage ? current.PageNumber : lastImage;
+                return extremes;
+            }, { firstImage: 999, lastImage: 0 });
+            console.log(firstImage, lastImage);
+            const images = data.map(image => [image.PageNumber, { imageID: image.ImageID, url: image.ImageURL, pageData: image.PageData, rotation: image.Rotation }]);
+            const patentImages = images && new Map(images);
             this.setState({
-                patentImages: images && new Map(images),
-                currentImage: images && images[0][0]
+                patentImages,
+                firstImage,
+                lastImage,
+                currentImage: firstImage,
+                rotation: patentImages.get(firstImage).rotation
             });
         });
         ipcRenderer.on('resize', (event, { width, height }) => {
             console.log(`got new window size width:${width} height:${height}`);
-            this.setState({ windowSize: { width, height } })
-        })
+            this.setState({ windowSize: { width, height } });
+        });
+    }
+
+    changeRotation = e => {
+        const cycle = {
+            0: 270,
+            90: 0,
+            180: 90,
+            270: 180
+        };
+        const currentRecord = this.state.patentImages.get(this.state.currentImage);
+        const rotation = cycle[this.state.rotation];
+        console.log(`rotating image ID ${currentRecord.imageID} from ${this.state.rotation} to ${rotation}`);
+        ipcRenderer.send('rotate_image', rotation, currentRecord.imageID);
+        ipcRenderer.send('change_window_rotation');
+        // in the meantime
+        const patentImages = new Map([...this.state.patentImages]);
+        patentImages.set(this.state.currentImage, { ...currentRecord, rotation })
+        this.setState({ rotation, patentImages });
+    }
+
+    nextPage = e => {
+        console.log('got request to go to next image page');
+        const currentImage = this.state.currentImage !== this.state.lastImage ? this.state.currentImage + 1 : this.state.currentImage;
+        const rotation = this.state.patentImages.get(currentImage).rotation;
+        if ((this.state.rotation / 90) % 2 !== (rotation / 90) % 2) ipcRenderer.send('change_window_rotation');
+        const nextEnabled = currentImage !== this.state.lastImage;
+        const prevEnabled = true;
+        this.setState({ currentImage, rotation, prevEnabled, nextEnabled });
+    }
+
+    prevPage = e => {
+        console.log('got request to go to prev image page');
+        const currentImage = this.state.currentImage !== this.state.firstImage ? this.state.currentImage - 1 : this.state.currentImage;
+        const rotation = this.state.patentImages.get(currentImage).rotation;
+        if ((this.state.rotation / 90) % 2 !== (rotation / 90) % 2) ipcRenderer.send('change_window_rotation');
+        const prevEnabled = currentImage !== this.state.firstImage;
+        const nextEnabled = true;
+        this.setState({ currentImage, rotation, prevEnabled, nextEnabled });
+    }
+
+    reportViewport = (width, height) => {
+        console.log(`viewport wants new width:${width} and height:${height}`);
+        console.log(`requesting new size ${width} x ${height + CONTROL_HEIGHT + 5}`);
+        ipcRenderer.send('request_resize', width, height + CONTROL_HEIGHT + 5);
     }
 
     render({ }, { }) {
         return (
             <div>
-                {this.state.currentImage ? < PatentImage
-                    imageData={this.state.patentImages}
-                    showPage={this.state.currentImage}
-                    rotation={90}
-                    width={this.state.windowSize.width}
-                /> : <div />}
+                <div style={{ height: `${CONTROL_HEIGHT}px`, position: 'fixed' }}>
+                    <button onClick={this.changeRotation}>Rotate</button>
+                    <button disabled={!this.state.nextEnabled} onClick={this.nextPage}>Next</button>
+                    <button disabled={!this.state.prevEnabled} onClick={this.prevPage}>Prev</button>
+                </div>
+                <div style={{ paddingTop: `${CONTROL_HEIGHT}px`, margin: '0px' }}>
+                    {this.state.currentImage ? < PatentImage
+                        imageData={this.state.patentImages}
+                        showPage={this.state.currentImage}
+                        windowSize={this.state.windowSize}
+                        controlAreaHeight={CONTROL_HEIGHT}
+                        rotation={this.state.rotation}
+                        reportViewport={this.reportViewport}
+                    /> : <div />}
+                </div>
             </div>
         );
     }
@@ -55,4 +122,4 @@ class PatentImages extends Component {
 
 
 
-render(<PatentImages />, document.getElementById('patentImages'))
+render(<PatentImages />, document.getElementById('patentImage'))
